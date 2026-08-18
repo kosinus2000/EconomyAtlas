298442ff1deac9d2239fe4d871554f9bf376a347
kosinus2000 <hubert.kosiorowski1@gmail.com>
2026-08-18 21:41:01 +0200

    DONE: -preparation for population model

---
+++ EconomyAtlas/src/data.py
@@ -0,0 +1,45 @@
+﻿from dataclasses import dataclass
+from enum import Enum
+
+class AdministrationUnit(Enum):
+
+    MIASTO = 'miasto'
+    GMINA_WIEJSKA = 'gmina wiejska'
+    GMINA_MIEJSKO_WIEJSKA = 'gmina miejsko-wiejska'
+    MIASTO_W_GMINIE = 'miasto w gminie miejsko-wiejskiej'
+    OBSZAR_WIEJSKI_W_GMINIE = 'obszar wiejski w gminie miejsko-wiejskiej'
+
+@dataclass
+class EconomyAtlasBase:
+    name: str
+    area: float
+    population: int
+    pkb: float
+
+@dataclass
+class Voivodeship(EconomyAtlasBase):
+    region_id: int
+
+
+@dataclass
+class Powiat(EconomyAtlasBase):
+    powiat_id: int
+    region_id: int
+
+
+
+@dataclass
+class AdministrationArea(EconomyAtlasBase):
+    administration_area_id: int
+    administration_area_id: int
+    powiat_id: int
+    type: AdministrationUnit
+    location: tuple[float, float]
+
+
+
+
+
+
+
+
